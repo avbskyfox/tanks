c399a05bce7a3116a1e5ba2aabac48080bf21048
Alex Bubnov <avb_it@outlook.com>
2016-12-07 00:28:08 +0500

    Create project

---
+++ android/src/com/avb/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.avb.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.avb.game.Tank;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Tank(), config);
+	}
+}
